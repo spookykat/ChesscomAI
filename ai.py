f3acc064466d08492e8c08e823fa17a28db0ac34
Kamiel Brees <kamiel.brees@gmail.com>
2021-10-29 23:18:02 +0200

    added minimax algorithm

--- ai.py
+++ ai.py
@@ -1,18 +1,21 @@
 import chess
+import copy
+import math
+
 class Ai:
-    def __init__(self, board):
-        self.board = board
+    def __init__(self):
+        pass
 
     def GetMoves(self):
         return self.board.legal_moves
 
-    def Evaluate(self):
+    def EvaluateScore(self, board):
         evaluation = 0
         cnt=0
         for square in chess.SQUARES:
             cnt += 1
-            if self.board.piece_at(square) != None:
-                piece = self.board.piece_at(square)
+            if board.piece_at(square) != None:
+                piece = board.piece_at(square)
                 worth = 0
 
                 if piece.piece_type == 1 : #Pawn
@@ -23,12 +26,44 @@ class Ai:
                     worth = 5
                 elif piece.piece_type == 5: #Queen
                     worth = 9
+                elif piece.piece_type == 6: #King
+                    worth = 900
 
 
-                if self.board.turn == piece.color:
+                if board.turn == piece.color:
                     evaluation += worth
                 else:
                     evaluation -= worth
 
         return evaluation
 
+    def minimax(self, depth, player, board):
+        board.turn = player
+        children = list(board.legal_moves)
+        best_move = None
+        if depth == 0:
+            return best_move, self.EvaluateScore(board)
+        if player:
+            max_eval = -math.inf
+            for child in children:
+                print(child)
+                board_copy = copy.deepcopy(board)
+                board_copy.push(child)
+                current_eval = self.minimax(depth - 1, False, board_copy)[1]
+                if current_eval > max_eval:
+                    max_eval = current_eval
+                    best_move = child
+            return best_move, max_eval
+        
+        else:
+            min_eval = math.inf
+            for child in children:
+                print(child)
+                board_copy = copy.deepcopy(board)
+                board_copy.push(child)
+                current_eval = self.minimax(depth - 1, True, board_copy)[1]
+                if current_eval < min_eval:
+                    min_eval = current_eval
+                    best_move = child
+            return best_move, min_eval
+        
--- test2.py
+++ test2.py
@@ -1,7 +1,9 @@
 import ai
 import chess
 
-board = chess.Board()
-AI = ai.Ai(board)
+board = chess.Board("rnb1k1nr/pppp1ppp/8/2b2q2/4p1N1/6P1/PPPPPP1P/RNBQKB1R w KQkq - 8 7")
 
-print(AI.Evaluate())
+Ai1 = ai.Ai()
+print(Ai1.minimax(4,True,board))
+
+print(board)
