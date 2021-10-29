5460f89b777ddfde368c9727eb710816279607b7
Kamiel Brees <kamiel.brees@gmail.com>
2021-10-29 09:51:09 +0200

    added Evaluation of board

--- ai.py
+++ ai.py
@@ -14,8 +14,15 @@ class Ai:
             if self.board.piece_at(square) != None:
                 piece = self.board.piece_at(square)
           
-                if piece.color:
-                    evaluation += 20
+                if self.board.turn == piece.color:
+                    if piece.piece_type == 1 : #Pawn
+                        evaluation += 1
+                    elif piece.piece_type == 2 or piece.piece_type == 3 : #Bishop or knight
+                        evaluation += 3
+                    elif piece.piece_type == 4: #Rook
+                        evaluation += 5
+                    elif piece.piece_type == 5: #Queen
+                        evaluation += 9
 
         return evaluation
 
