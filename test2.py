2b5a99e1933f0e4a209988948cf7b7f747334a04
Kamiel Brees <kamiel.brees@gmail.com>
2021-10-30 17:45:17 +0200

    small test

--- test2.py
+++ test2.py
@@ -1,9 +1,9 @@
 import ai
 import chess
 
-board = chess.Board("rnb1k1nr/pppp1ppp/8/2b2q2/4p1N1/6P1/PPPPPP1P/RNBQKB1R w KQkq - 8 7")
+board = chess.Board("rnbqkbnr/p1p3pp/p4p2/3pp3/4P3/7N/PPPP1PPP/RNBQK2R w KQkq - 0 5")
 
 Ai1 = ai.Ai()
 print(Ai1.minimax(4,True,board))
-
+print(Ai1.EvaluateScore(board))
 print(board)
