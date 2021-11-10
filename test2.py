a2a4fc8ca9a4a408b7cf22c7b3eb129c16bc144f
Kamiel Brees <kamielbrees@arcadiascholen.be>
2021-11-10 11:23:46 +0100

    test commit

--- test2.py
+++ test2.py
@@ -6,3 +6,4 @@ import piecesquaretables
 board = chess.Board("r1bqkbnr/ppp1pppp/2n5/3p4/8/2N2N2/PPPPPPPP/R1BQKB1R w KQkq - 2 3")
 Ai1 = ai.Ai()
 print(Ai1.minimax(3,-math.inf, math.inf, True,board)[0])
+#testcommit
